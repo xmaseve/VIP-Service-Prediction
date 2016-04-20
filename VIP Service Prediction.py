e7a1b7f681884d1c4f5d753cdda9eb0fb8676f3d
Yi Qi <qiyi1224@gmail.com>
2016-04-19 22:31:31 -0400

    Update VIP Service Prediction.py

--- VIP Service Prediction.py
+++ VIP Service Prediction.py
@@ -7,10 +7,9 @@ Created on Tue Apr 19 18:23:13 2016
 
 import json
 import csv
-import numpy as np
 import pandas as pd
 
-b= pd.read_csv('user.csv')
+
 
 #read json
 data = []
@@ -18,11 +17,7 @@ with open('user.json') as f:
     for line in f:
         data.append(json.loads(line))
 
-a = csv.writer(open('user1.csv','w'))
-a.writerow['appfile','uid','app','label','activity_time','category_id']
-                   
-
-
+#convert json to csv
 filecsv = csv.writer(open('user.csv','w'))
 count = 0
 for line in data:
@@ -33,20 +28,5 @@ for line in data:
     filecsv.writerow((line.values())
 
 
-a = np.loadtxt('user.csv')
-
-'''
-columns = map(lambda x: x.keys(), data)
-columns = columns[0]
-filecsv.writerow(columns)
-
-for line in data:
-    filecsv.writerow( map( lambda x: line.get( x, "" ), columns ) )
-
-
-with open('user.csv', 'wb') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=',')
-
-with open('eggs.csv', 'wb') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=' ')
-'''
+#read csv
+df= pd.read_csv('user.csv')
